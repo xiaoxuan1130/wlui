18851be725b0893409682e17536ac6656ac5ea00
1024046894@qq.com <1024046894@qq.com>
2021-08-31 11:05:59 +0800

    添加无头设置

--- Testcase/conftest.py
+++ Testcase/conftest.py
@@ -3,9 +3,19 @@ import time
 import pytest
 from selenium import webdriver
 
+from selenium.webdriver.chrome.options import Options
+
+
 @pytest.fixture(scope="session")
 def login():
-    driver=webdriver.Chrome()
+    #driver=webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+
     driver.get("http://192.168.10.164:8420/#/login")
     driver.find_elements_by_css_selector("div[class='el-form-item__content']>div>input")[0].send_keys("liyuexuan")
     # driver.get("http://192.168.10.182:8000//#/login")
